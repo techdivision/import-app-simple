ac39c9176c698d2d76b78a36cdabdafcb4fbf629
wagnert <t.wagner@techdivision.com>
2017-06-16 15:20:38 +0200

    Rename class SynteticServiceKeys to DependencyInjectionKeysi + DependencyInjectionKeys now extends same class from techdivision/import library

---
+++ src/Utils/DependencyInjectionKeys.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * TechDivision\Import\App\Utils\DependencyInjectionKeys
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import-app-simple
+ * @link      http://www.techdivision.com
+ */
+
+namespace TechDivision\Import\App\Utils;
+
+/**
+ * A utility class for the DI service keys.
+ *
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import-app-simple
+ * @link      http://www.techdivision.com
+ */
+class DependencyInjectionKeys extends \TechDivision\Import\Utils\DependencyInjectionKeys
+{
+
+    /**
+     * The key for the application service.
+     *
+     * @var string
+     */
+    const SIMPLE = 'import_app_simple.simple';
+}
